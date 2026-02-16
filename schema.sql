84b64a03e28e5269185abd8042fd30e85b6a5768
Alexey Milovidov <milovidov@clickhouse.com>
2026-02-16 22:18:44 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -5,8 +5,8 @@
 --   clickhouse-client < schema.sql
 --
 -- Step 2: Load data:
---   clickhouse-local -q "SELECT _path AS path, json AS data FROM file('~/.claude/projects/*/*.jsonl', 'JSONAsString') WHERE isValidJSON(json) SETTINGS input_format_allow_errors_ratio=0.1 FORMAT Native" | clickhouse-client -q "INSERT INTO claude_code.raw FORMAT Native"
---   clickhouse-local -q "SELECT _path AS path, json AS data FROM file('~/.claude/history.jsonl', 'JSONAsString') WHERE isValidJSON(json) SETTINGS input_format_allow_errors_ratio=0.1 FORMAT Native" | clickhouse-client -q "INSERT INTO claude_code.raw FORMAT Native"
+--   clickhouse-local -q "SELECT _path AS path, json AS data FROM file('$HOME/.claude/projects/*/*.jsonl', 'JSONAsString') WHERE isValidJSON(json) SETTINGS input_format_allow_errors_ratio=0.1 FORMAT Native" | clickhouse-client -q "INSERT INTO claude_code.raw FORMAT Native"
+--   clickhouse-local -q "SELECT _path AS path, json AS data FROM file('$HOME/.claude/history.jsonl', 'JSONAsString') WHERE isValidJSON(json) SETTINGS input_format_allow_errors_ratio=0.1 FORMAT Native" | clickhouse-client -q "INSERT INTO claude_code.raw FORMAT Native"
 
 CREATE DATABASE IF NOT EXISTS claude_code;
 
